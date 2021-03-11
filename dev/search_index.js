702423e157b9a1d25e178135c096512ce60a740d
Documenter.jl <documenter@juliadocs.github.io>
2021-03-11 02:44:25 +0000

    build based on 81ea1f5

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[]
+[{"location":"","page":"DemoActions","title":"DemoActions","text":"CurrentModule = DemoActions","category":"page"},{"location":"#DemoActions","page":"DemoActions","title":"DemoActions","text":"","category":"section"},{"location":"","page":"DemoActions","title":"DemoActions","text":"","category":"page"},{"location":"","page":"DemoActions","title":"DemoActions","text":"Modules = [DemoActions]","category":"page"}]
 }
