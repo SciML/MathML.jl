49ab9e0b42596781cf5032d161bfabccbbb034dc
Anand <anandj@uchicago.edu>
2021-03-16 22:27:15 -0700

    remove nonexistent export, trees compat

--- src/MathML.jl
+++ src/MathML.jl
@@ -8,5 +8,5 @@ include("utils.jl")
 export extract_mathml, mathml_to_nums, @xml_str, @MathML_str
 export parse_node, parse_apply, parse_file, parse_str
 
-export parse_cn, parse_ci, parse_bvar, parse_lambda, parse_math, parse_piecewise
+export parse_cn, parse_ci, parse_bvar, parse_lambda, parse_piecewise
 end
