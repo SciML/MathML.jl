7d494322849a9f53d11f31c0629e2f46a73b568f
Anand <anandj@uchicago.edu>
2021-03-18 13:15:17 -0700

    fix include order to pass tests

--- src/MathML.jl
+++ src/MathML.jl
@@ -2,9 +2,9 @@ module MathML
 
 using EzXML, Symbolics, Statistics, IfElse, AbstractTrees
 
-include("maps.jl")
-include("parse.jl")
 include("utils.jl")
+include("parse.jl")
+include("maps.jl")
 
 export extract_mathml, mathml_to_nums, @xml_str, @MathML_str
 export parse_node, parse_apply, parse_file, parse_str
