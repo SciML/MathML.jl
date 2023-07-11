a7b65012175c8e947859dfef920d959c4ad4f06e
oscarddssmith <oscar.smith@juliacomputing.com>
2023-07-11 13:09:08 -0400

    fix test

--- test/generate.jl
+++ test/generate.jl
@@ -1,3 +1,5 @@
 ex = :(1+2+x+y*z*num^4)
-str =  print(symbol_to_MathML(ex))
+io = IOBuffer()
+print(io, symbol_to_MathML(ex))
+str = String(take!(io))
 @test str == "<apply><cn>1</cn><cn>2</cn><ci>x</ci><apply><ci>y</ci><ci>z</ci><apply><ci>num</ci><cn>4</cn></apply></apply></apply>"
