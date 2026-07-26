5762af63543a036fc3e2409b7abc56a81a4dfd0b
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-26 06:44:10 -0400

    Migrate MathML QA and public API docs
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,16 @@
 using Documenter, MathML
 
-makedocs(sitename = "My Documentation")
+DocMeta.setdocmeta!(MathML, :DocTestSetup, :(using MathML, EzXML); recursive = true)
+
+makedocs(
+    sitename = "MathML.jl",
+    modules = [MathML],
+    checkdocs = :exports,
+    doctest = true,
+    pages = ["Home" => "index.md", "API" => "api.md"],
+)
 
 deploydocs(
-    repo = "github.com/anandijain/MathML.jl.git",
+    repo = "github.com/SciML/MathML.jl.git",
     devbranch = "main"
 )
